e686079da7c8a35f32306e7c790ae21926d81f99
bharat joshi <bharatjoshi.p@gmail.com>
2019-06-27 13:02:50 +1000

    refactor: minor refactor

--- init.el
+++ init.el
@@ -1,9 +1,5 @@
 (setq gc-cons-threshold (* 50 1000 1000))
 
-(require 'package)
-(setq package-enable-at-startup nil)
-(package-initialize)
-
 (add-hook 'after-init-hook (lambda ()
     (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
     (load-file (expand-file-name "config.el" user-emacs-directory))
@@ -11,11 +7,15 @@
     (setq gc-cons-threshold (* 2 1000 1000))
   ))
 
-;; (add-hook 'emacs-startup-hook
-;;           (lambda ()
-;;             (message "Emacs ready in %s with %d garbage collections."
-;;                      (format "%.2f seconds"
-;;                              (float-time
-;;                               (time-subtract after-init-time before-init-time)))
-;;                      gcs-done)))
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(package-initialize)
 
