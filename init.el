57ffa659a87742ba064e8b123e4312066efd2f4a
bharat joshi <bharatjoshi.p@gmail.com>
2019-06-29 11:23:02 +1000

    feat: custom variables in custom file

--- init.el
+++ init.el
@@ -19,3 +19,6 @@
 (setq package-enable-at-startup nil)
 (package-initialize)
 
+(setq-default custom-file (expand-file-name ".custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
