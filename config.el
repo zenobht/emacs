3c6c083175c5505e3ad4b863229cc9fe4479593a
bharat joshi <bharatjoshi.p@gmail.com>
2019-07-18 15:06:45 +1000

    feat: add quick empty buffer

--- config.el
+++ config.el
@@ -218,6 +218,22 @@
   (define-key evil-visual-state-map (kbd "<") 'my/visual-shift-left)
   )
 
+(defun my/new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf
+    ))
+
 (bind-keys*
  ( "M-}" . my/next-buffer)
  ( "M-{" . my/previous-buffer)
@@ -227,6 +243,7 @@
  ( "M-v" . (lambda () (interactive) (message "Pasted from clipboard")))
  ( "M-c" . my/copy-to-clipboard)
  ( "M-e" . elfeed)
+ ( "M-n" . my/new-empty-buffer)
  )
 
 (unbind-key "C-l" global-map)
